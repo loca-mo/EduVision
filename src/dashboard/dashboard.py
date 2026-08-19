77447d748baaa61b82db7bf49504e6e96920581f
Malak Hossam <malak223101352@nmu.edu.eg>
2026-08-19 00:01:23 +0000

    Update dashboard and fix bugs

---
+++ src/dashboard/dashboard.py
@@ -0,0 +1,128 @@
+#######################
+# Import libraries
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="Dashboard Template", # Replace this with your Project's Title
+    page_icon="assets/icon.png", # You may replace this with a custom icon or emoji related to your project
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+#######################
+
+# Initialize page_selection in session state if not already set
+if 'page_selection' not in st.session_state:
+    st.session_state.page_selection = 'about'  # Default page
+
+# Function to update page_selection
+def set_page_selection(page):
+    st.session_state.page_selection = page
+
+# Sidebar
+with st.sidebar:
+
+    # Sidebar Title (Change this with your project's title)
+    st.title('Dashboard Template')
+
+    # Page Button Navigation
+    st.subheader("Pages")
+
+    if st.button("About", use_container_width=True, on_click=set_page_selection, args=('about',)):
+        st.session_state.page_selection = 'about'
+    
+    if st.button("Dataset", use_container_width=True, on_click=set_page_selection, args=('dataset',)):
+        st.session_state.page_selection = 'dataset'
+
+    if st.button("EDA", use_container_width=True, on_click=set_page_selection, args=('eda',)):
+        st.session_state.page_selection = "eda"
+
+    if st.button("Data Cleaning / Pre-processing", use_container_width=True, on_click=set_page_selection, args=('data_cleaning',)):
+        st.session_state.page_selection = "data_cleaning"
+
+    if st.button("Machine Learning", use_container_width=True, on_click=set_page_selection, args=('machine_learning',)): 
+        st.session_state.page_selection = "machine_learning"
+
+    if st.button("Prediction", use_container_width=True, on_click=set_page_selection, args=('prediction',)): 
+        st.session_state.page_selection = "prediction"
+
+    if st.button("Conclusion", use_container_width=True, on_click=set_page_selection, args=('conclusion',)):
+        st.session_state.page_selection = "conclusion"
+
+    # Project Members
+    st.subheader("Members")
+    st.markdown("1. Elon Musk\n2. Jeff Bezos\n3. Sam Altman\n4. Mark Zuckerberg")
+
+#######################
+# Data
+
+# Load data
+dataset = pd.read_csv("data/IRIS.csv")
+
+#######################
+
+# Pages
+
+# About Page
+if st.session_state.page_selection == "about":
+    st.header("ℹ️ About")
+
+    # Your content for the ABOUT page goes here
+
+# Dataset Page
+elif st.session_state.page_selection == "dataset":
+    st.header("📊 Dataset")
+
+    st.write("IRIS Flower Dataset")
+    st.write("")
+
+    # Your content for your DATASET page goes here
+
+# EDA Page
+elif st.session_state.page_selection == "eda":
+    st.header("📈 Exploratory Data Analysis (EDA)")
+
+
+    col = st.columns((1.5, 4.5, 2), gap='medium')
+
+    # Your content for the EDA page goes here
+
+    with col[0]:
+        st.markdown('#### Graphs Column 1')
+
+
+    with col[1]:
+        st.markdown('#### Graphs Column 2')
+        
+    with col[2]:
+        st.markdown('#### Graphs Column 3')
+
+# Data Cleaning Page
+elif st.session_state.page_selection == "data_cleaning":
+    st.header("🧼 Data Cleaning and Data Pre-processing")
+
+    # Your content for the DATA CLEANING / PREPROCESSING page goes here
+
+# Machine Learning Page
+elif st.session_state.page_selection == "machine_learning":
+    st.header("🤖 Machine Learning")
+
+    # Your content for the MACHINE LEARNING page goes here
+
+# Prediction Page
+elif st.session_state.page_selection == "prediction":
+    st.header("👀 Prediction")
+
+    # Your content for the PREDICTION page goes here
+
+# Conclusions Page
+elif st.session_state.page_selection == "conclusion":
+    st.header("📝 Conclusion")
+
+    # Your content for the CONCLUSION page goes here
